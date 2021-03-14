ee1c78cf6a3564cdfdaa3add5ca1dc6e795f2c80
landtlord <andy.landtsheer@pandora.be>
2021-03-14 13:22:39 +0100

    Initial commit

---
+++ src/main/java/be/landtlord/legocollection/LegocollectionApplication.java
@@ -0,0 +1,13 @@
+package be.landtlord.legocollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegocollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegocollectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/landtlord/legocollection/LegocollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package be.landtlord.legocollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegocollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
